5a68295ae502add2d4e8d2aca2415397ec8f2adb
Amit Jha <amit.jha@pratikshat.com>
2022-08-29 11:03:08 +0530

    day10

---
+++ lib/models/catalog.dart
@@ -0,0 +1,12 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String imageUrl;
+//name constructor 
+  Item(this.id, this.name, this.desc, this.price, this.color, this.imageUrl);
+
+}
+final product=[Item("1", "iPhone13Pro", "Apple iPhone13Pro generator",77777, "#33505a", "")];
